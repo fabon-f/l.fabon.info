3ea78f4a01d4f0fe3fd7f4c64605035cb0435ea7
fabon <syobon.hinata.public@gmail.com>
2022-10-21 20:22:42 +0900

    Create simple page using vite-plugin-ssr

---
+++ pages/index.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function Page() {
+  return (
+    <>
+      <h1>l.fabon.info</h1>
+      <p>L stands for...<br/>
+        <span>Library, Language, Literature, Lyrics, Letters, Laboratory, etc.</span>
+      </p>
+    </>
+  )
+}
