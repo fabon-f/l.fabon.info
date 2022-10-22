50d4dfeeb4d67afbfa766fe850bd3a3237658c64
fabon <syobon.hinata.public@gmail.com>
2022-10-23 04:55:06 +0900

    Improve markup

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export function Page() {
   return (
-    <>
+    <main role="main">
       <h1>l.fabon.info</h1>
       <p>L stands for...<br/>
         <span>Library, Language, Literature, Lyrics, Letters, Laboratory, etc.</span>
       </p>
-    </>
+    </main>
   )
 }
