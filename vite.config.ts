e6a0024c11a91c2986205ca70befecb8f0a6094e
fabon <syobon.hinata.public@gmail.com>
2022-10-23 04:48:04 +0900

    Add linaria to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 import type { UserConfig } from 'vite'
+import linaria from '@linaria/rollup';
 
 const config: UserConfig = {
   plugins: [
+    linaria(),
     react(),
     ssr({
       prerender: true
