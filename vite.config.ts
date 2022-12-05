77aca7e0b7ca5a077009abf7bfe7129a3f7105dc
fabon <syobon.hinata.public@gmail.com>
2022-12-06 08:40:56 +0900

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import preact from '@preact/preset-vite'
 import ssr from 'vite-plugin-ssr/plugin'
 import type { UserConfig } from 'vite'
-import linaria from '@linaria/rollup';
+import linaria from '@linaria/vite';
 
 const config: UserConfig = {
   plugins: [
